42e5f3d1453c9a86e40f9075ec5c772b3eb5d5b0
Michael Sorhaindo <pmsorhaindo@gmail.com>
2014-11-05 12:46:57 +0000

    too much for jsbin pre require

---
+++ horizontalBarChart.js
@@ -0,0 +1,82 @@
+define([
+    'jquery',
+    'underscore',
+
+], function(
+    $,
+    _
+){
+    function horizontalBarChart(data,element){
+      
+      var animationSeconds = 1; // Default animation length 1.
+      
+      // mocking data.
+      data = [{
+        name: "Executive",
+        percentage: 65
+      }, {
+        name: "Scientist&nbsp;&amp;&Researcher",
+        percentage: 21
+      }, {
+        name: "Artist",
+        percentage: 14
+      }];
+
+      var grabPercentage = function(x) {
+        return x.percentage;
+      };
+
+      var grabName = function(x){
+        return x.name;
+      }
+
+      var metricValues = data.map(grabPercentage); //.concat(intrObjArr.map(grabPercentage));
+      var barNames = data.map(grabName);
+
+      var incFrameFunc = function(x) {
+        return Math.ceil(x / (animationSeconds * 60 /*approx. fps*/ ));
+      };
+      var displayedValuesArr = Array.apply(null, new Array(metricValues.length)).map(Number.prototype.valueOf, 0);
+      var incrementsPerFrameArr = metricValues.map(incFrameFunc);
+
+
+      var start = Date.now();
+
+      requestAnimationFrame(frame);
+
+      function frame() {
+        // calculate next values by adding each corresponding value arrays incrementsPerFrameArr to displayedValuesArr
+        var nextValuesArr = _.zip(displayedValuesArr, incrementsPerFrameArr).map(function(x) {
+          return x.reduce(function(previousValue, currentValue, index, array) {
+            return Number(previousValue) + Number(currentValue);
+          });
+        });
+        if (allBarsComplete(nextValuesArr)) {
+          nextValuesArr = metricValues;
+        } else {
+          requestAnimationFrame(frame);
+        }
+
+        $("#bar1").css("width", nextValuesArr[0]+ "%");
+        $("#bar2").css("width", nextValuesArr[1] + "%");
+        $("#bar3").css("width", nextValuesArr[2] + "%");
+        displayedValuesArr = nextValuesArr;
+      }
+
+      function allBarsComplete(nextValuesArr) {
+        var complete = true;
+        for (var i = 0; i < metricValues.length; i++) {
+          if (nextValuesArr[i] < metricValues[i]) {
+            complete = false;
+          }
+          else {
+            nextValuesArr[i] = metricValues[i];
+          }
+        }
+        return complete;
+      }
+
+    }
+
+    return horizontalBarChart;
+});
