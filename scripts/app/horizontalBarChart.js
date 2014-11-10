bee5a239b9b48ea5bf1b502c32f160bf7fa71788
Michael Sorhaindo <pmsorhaindo@gmail.com>
2014-11-10 15:51:41 +0000

    single var statements

--- scripts/app/horizontalBarChart.js
+++ scripts/app/horizontalBarChart.js
@@ -8,21 +8,16 @@ define([
 ) {
   function horizontalBarChart(data, title, chartID, elem) {
 
-    var animationSeconds = 1; // Default animation length 1.
-    var color = '#55ACEE';
-
-    var grabPercentage = function(x) {
-      return x.percentage;
-    };
-
-    var grabName = function(x) {
-      return x.name;
-    }
-
-    var metricValues = data.map(grabPercentage);
-    var normalizedPercentageArr = normalizePercentages(metricValues);
-    var barNames = data.map(grabName);
-    var numberOfBars = barNames.length;
+    var animationSeconds = 1,// Default animation length 1.
+      color = '#55ACEE',
+      metricValues = data.map(grabPercentage),
+      normalizedPercentageArr = normalizePercentages(metricValues),
+      barNames = data.map(grabName),
+      numberOfBars = barNames.length,
+      displayedValuesArr = Array.apply(null, new Array(normalizedPercentageArr.length))
+        .map(Number.prototype.valueOf, 0),
+      incrementsPerFrameArr = normalizedPercentageArr.map(incFrameFunc),
+      start = Date.now();
 
     setHeading(elem);
 
@@ -33,29 +28,32 @@ define([
       var $percentage = $('<div></div>').text(metricValues[i] + '%')
         .addClass('percentage');
       var $bar = $('<div></div>')
-        //.css("float", "right")
         .css("width", 0)
         .css("background-color", color)
         .attr("id", chartID + 'Bar' + i)
         .text(String.fromCharCode(160));
 
       var $barLabel = $('<div></div>').addClass('barLabel smalltext textellipsis cellpadding').text(barNames[i]);
+      
       $lineHolder.append($barLabel);
       elem.addClass('fivecol');
       elem.append($lineHolder.append($profession.append($percentage).append($child.append($bar))));
     }
 
-    var incFrameFunc = function(x) {
+    requestAnimationFrame(frame);
+
+
+    function incFrameFunc(x) {
       return Math.ceil(x / (animationSeconds * 60 /*approx. fps*/ ));
     };
-    var displayedValuesArr = Array.apply(null, new Array(normalizedPercentageArr.length))
-      .map(Number.prototype.valueOf, 0);
-    var incrementsPerFrameArr = normalizedPercentageArr.map(incFrameFunc);
 
+    function grabPercentage(x) {
+      return x.percentage;
+    };
 
-    var start = Date.now();
-
-    requestAnimationFrame(frame);
+    function grabName(x) {
+      return x.name;
+    };
 
     function frame() {
       // calculate next values by adding each corresponding value arrays incrementsPerFrameArr to displayedValuesArr
@@ -75,18 +73,15 @@ define([
       }
 
       displayedValuesArr = nextValuesArr;
-    }
+    };
 
     function setHeading(elem) {
       // Enforce a vertical separator
       elem.append($('<hr />').css('color', '#d3d3d5 c00').css('background-color', '#d3d3d5 '));
-
-      // Print Charts title?
+      // Print Charts title
       elem.append($('<div></div>').addClass('largetext smallpadding-vertical')
-        //.css('padding-bottom', '12px')
-        //.css('padding-top', '6px')
         .text(title.toUpperCase()));
-    }
+    };
 
     function allBarsComplete(nextValuesArr) {
       var complete = true;
@@ -98,18 +93,18 @@ define([
         }
       }
       return complete;
-    }
+    };
 
     function normalizePercentages(percentagesArr) {
-      var maxVal = Math.max.apply(null, percentagesArr);
-      var newPerc = 100 / maxVal;
-      var normalizedPercentageArray = percentagesArr.map(function(val) {
+      var maxVal = Math.max.apply(null, percentagesArr),
+        newPerc = 100 / maxVal,
+        normalizedPercentageArray = percentagesArr.map(function(val) {
         return newPerc * val
       });
       return normalizedPercentageArray
-    }
+    };
 
-  }
+  };
 
   return horizontalBarChart;
 });
